c06e159b4a511b193c7dac043f7b411a271554b8
Bojidar Boradjiev -X (bboradji - ONPROCESS TECHNOLOGY INC at Cisco) <bboradji@cisco.com>
2021-01-12 11:26:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  
+  return (
+    <div className='grid-container'>
+        <header>
+            <a href='/'>Shopping Cart</a>
+        </header>
+        <main>
+          product List
+        </main>
+        <footer>
+          this is the Footer
+        </footer>
+    </div>
+  );
+};
+
+export default App;
